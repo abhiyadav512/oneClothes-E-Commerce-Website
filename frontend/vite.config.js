bcc0aeb4cb8953d472b530bfa03b06f411d3d440
abhiyadav512 <abhihsekcyadav9594@gmail.com>
2025-06-21 17:51:08 +0530

    add backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        // target: "http://localhost:3000",
+        target: "https://oneclothes.onrender.com",
       },
     },
   },
