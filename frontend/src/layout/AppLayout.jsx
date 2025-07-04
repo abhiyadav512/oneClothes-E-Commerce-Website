c2bd3891dd7c439774ad3b9299e7537cd34b2eb6
abhiyadav512 <abhihsekcyadav9594@gmail.com>
2025-07-04 10:35:16 +0530

    add Analytics

--- frontend/src/layout/AppLayout.jsx
+++ frontend/src/layout/AppLayout.jsx
@@ -2,6 +2,7 @@ import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react';
 
 const AppLayout = () => {
     return (
@@ -9,6 +10,7 @@ const AppLayout = () => {
             <Header />
             <main>
                 <Outlet />
+                <Analytics/>
             </main>
             <Footer />
         </div>
